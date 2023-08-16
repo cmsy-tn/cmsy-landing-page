cf5dd99c108b3e701328609082fdc97f8b9a8c62
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-16 21:00:31 +0100

    Main section

---
+++ src/components/MainSection.tsx
@@ -0,0 +1,26 @@
+export default function MainSection() {
+    return (
+        <div className='mt-8 min-h-full w-full py-4 relative'>
+            <main className='max-w-[var(--max-content-width)] mx-auto z-20 relative'
+            >
+                <h3 className="uppercase text-lg mb-8 text-[var(--offset-text)]">Unleash</h3>
+                <h1 className='text-white text-6xl text-left font-["Limelight"] capitalize'>
+                    Create extraordinary portfolios with CMSY in minutes.
+                </h1>
+            </main>
+
+            <img
+                src="src/assets/main_section_graphic.webp"
+                alt="hero section backgrond"
+                style={{
+                    display: 'block',
+                    width: '600px',
+                    position: "absolute",
+                    right: '0',
+                    top: "0",
+                    zIndex: 1,
+                }}
+            />
+        </div>
+    )
+}
