ebff445312e340dd9787abd88c5c78ebc36bde1f
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-16 20:39:48 +0100

    Hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,35 @@
+import concatClassNames from "../utils/ConcatClasses";
+
 export default function HeroSection() {
     return (
-        <div>HeroSection</div>
+        <div className={concatClassNames(
+            'h-[100vh] w-full',
+            'flex flex-row items-center justify-center',
+            'relative'
+        )}>
+
+            <img
+                src="src/assets/hero_section_graphic.webp"
+                alt="hero section backgrond"
+                style={{
+                    zIndex: 2,
+                    width: '700px',
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}
+            />
+
+            <main className="max-w-[var(--max-content-width)] mx-auto z-20">
+                <h1 className={concatClassNames(
+                    'text-white text-[6vw] text-center font-["Limelight"] capitalize'
+                )}
+                >
+                    revolutionize your portfolio game
+                </h1>
+            </main>
+
+        </div>
     )
 }
