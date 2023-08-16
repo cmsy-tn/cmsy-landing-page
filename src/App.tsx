1f086daa06529a7ceceb8776dacd343904f58cfa
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-16 20:01:32 +0100

    Landing page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import styled from "styled-components"
+// SECTIONS IMPORT
 import HeroSection from "./components/HeroSection"
 
 function App() {
   return (
-    <>
+    <LandingPageLayout>
       <HeroSection />
-    </>
+    </LandingPageLayout>
   )
 }
 
 export default App
+
+const LandingPageLayout = styled.section`
+  height: 100vh;
+  width: 100vw;
+  background: rgb(30,39,73);
+  background: linear-gradient(142deg, rgba(30,39,73,1) 9%, rgba(39,52,105,1) 93%);
+`;
