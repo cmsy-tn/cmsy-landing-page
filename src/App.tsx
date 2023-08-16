008430d5fb41bc10238cc24beb17eade3ddf773c
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-16 21:32:32 +0100

    background gradient update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import styled from "styled-components"
 // SECTIONS IMPORT
 import HeroSection from "./components/HeroSection"
 import MainSection from "./components/MainSection";
+import GuidesSection from "./components/guides/GuidesSection";
 
 function App() {
   return (
     <LandingPageLayout>
       <HeroSection />
       <MainSection />
+      <GuidesSection />
     </LandingPageLayout>
   )
 }
@@ -18,5 +20,5 @@ const LandingPageLayout = styled.section`
   min-height: 100vh;
   width: 100%;
   background: rgb(30,39,73);
-  background: linear-gradient(142deg, rgba(30,39,73,1) 9%, rgba(39,52,105,1) 93%);
+  background: linear-gradient(90deg, rgba(30,39,73,1) 9%, rgba(39,52,105,1) 93%);
 `;
