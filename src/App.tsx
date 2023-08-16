5fb7e53204756fba87f674ad43b236a0b38b2304
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-16 21:04:43 +0100

    minor styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 // SECTIONS IMPORT
 import HeroSection from "./components/HeroSection"
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
     <LandingPageLayout>
       <HeroSection />
+      <MainSection />
     </LandingPageLayout>
   )
 }
@@ -13,8 +15,8 @@ function App() {
 export default App
 
 const LandingPageLayout = styled.section`
-  height: 100vh;
-  width: 100vw;
+  min-height: 100vh;
+  width: 100%;
   background: rgb(30,39,73);
   background: linear-gradient(142deg, rgba(30,39,73,1) 9%, rgba(39,52,105,1) 93%);
 `;
